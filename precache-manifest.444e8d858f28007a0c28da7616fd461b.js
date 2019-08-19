4ac5d89ebd428f879c05e2ffacc5b32b69bef17a
Johnisaig <isaigmichael30@gmail.com>
2019-08-19 18:19:44 +0800

    Updates

---
+++ precache-manifest.444e8d858f28007a0c28da7616fd461b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83941a7012a1088ad313cad282ee875e",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "ba88f682d82258e0a2ce",
+    "url": "/Portfolio/static/css/2.3fdf68b5.chunk.css"
+  },
+  {
+    "revision": "8111bcb4ec7ca81f24c4",
+    "url": "/Portfolio/static/css/main.a7b5fda1.chunk.css"
+  },
+  {
+    "revision": "ba88f682d82258e0a2ce",
+    "url": "/Portfolio/static/js/2.9c05c2c2.chunk.js"
+  },
+  {
+    "revision": "8111bcb4ec7ca81f24c4",
+    "url": "/Portfolio/static/js/main.b711b795.chunk.js"
+  },
+  {
+    "revision": "6b64d540f1308ee928ad",
+    "url": "/Portfolio/static/js/runtime~main.132fbc8c.js"
+  },
+  {
+    "revision": "99abbc86039a471f7f79459e7bb19520",
+    "url": "/Portfolio/static/media/60751952_2346240132104460_6796298400275890176_n.99abbc86.jpg"
+  },
+  {
+    "revision": "57e97fc2d2ff74ae086810803052b0cd",
+    "url": "/Portfolio/static/media/React.57e97fc2.jpg"
+  },
+  {
+    "revision": "e6247fa7aa5492a28b72deadbe92d9a6",
+    "url": "/Portfolio/static/media/Stratpoint.e6247fa7.png"
+  },
+  {
+    "revision": "dcb1b92c98fcfc68602381dd13834861",
+    "url": "/Portfolio/static/media/download.dcb1b92c.jpg"
+  },
+  {
+    "revision": "6f2fb72201f0f6bfdea3c7af2fac8729",
+    "url": "/Portfolio/static/media/kel1.6f2fb722.jpg"
+  },
+  {
+    "revision": "f90f20b9ff8c68b7adfe4e3c2179dceb",
+    "url": "/Portfolio/static/media/meetup.f90f20b9.jpeg"
+  },
+  {
+    "revision": "84ea384c6e9853a63f3b9ae0ffa1527d",
+    "url": "/Portfolio/static/media/x.84ea384c.png"
+  }
+]);
